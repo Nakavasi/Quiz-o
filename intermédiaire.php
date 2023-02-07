1cf2e506ea871cccbf30723522e3e12a68e6735c
Nakavasi <maxancemangeret@gmail.com>
2023-02-07 12:17:44 +0100

    Règlage

---
+++ intermédiaire.php
@@ -0,0 +1,16 @@
+<?php
+echo "Bonjour encore";
+// $Utilisateur =[["email" => "maxancemangeret@gmail.com", "nom"=>"Nakavasi","mdp"=>"1234"]];
+// $pseudo ="Nakavasi";
+// $mail= "maxancemangeret@gmail.com";
+// $mdp= "1234";
+// for [$i=0;$i<sizeof($Utilisateur);$i++]{
+//     if ($mail == $Utilisateur[$i]["email"]){
+//         if ($pseudo == $Utilisateur[$i]["pseudo"]){
+//             if ($mdp == $Utilisateur[$i]["mdp"]){
+//                 header('Location: accueil.html');
+//             }
+//         }
+//     }
+// }
+?>
