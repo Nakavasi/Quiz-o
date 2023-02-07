517158235d06d02e6c5e8411d5a5d63797a7c286
Nakavasi <maxancemangeret@gmail.com>
2023-02-07 15:12:47 +0100

    Rajout du code PHP

--- intermédiaire.php
+++ intermédiaire.php
@@ -1,16 +1,28 @@
-<?php
-echo "Bonjour encore";
-// $Utilisateur =[["email" => "maxancemangeret@gmail.com", "nom"=>"Nakavasi","mdp"=>"1234"]];
-// $pseudo ="Nakavasi";
-// $mail= "maxancemangeret@gmail.com";
-// $mdp= "1234";
-// for [$i=0;$i<sizeof($Utilisateur);$i++]{
-//     if ($mail == $Utilisateur[$i]["email"]){
-//         if ($pseudo == $Utilisateur[$i]["pseudo"]){
-//             if ($mdp == $Utilisateur[$i]["mdp"]){
-//                 header('Location: accueil.html');
-//             }
-//         }
-//     }
-// }
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vérification</title>
+</head>
+<body>
+    <?php
+        $Utilisateur =[["email" => "maxancemangeret@gmail.com", "nom"=>"Nakavasi","mdp"=>"1234"]];
+        $pseudo= $_POST["prenom"];
+        $mdp= $_POST["password"];
+        $nonValide= TRUE;
+        for ($i=0;$i<sizeof($Utilisateur);$i++){
+            if ($pseudo == $Utilisateur[$i]["nom"] or $pseudo == $Utilisateur[$i]["email"]){
+                    if ($mdp == $Utilisateur[$i]["mdp"]){
+                        header('Location: accueil.html');
+                        $nonValide = False;
+                    }
+            }
+        }
+        if ($nonValide){
+            header('Location: connexion.html');
+        }
+    ?>
+</body>
+</html>
--- intermédiaire1.php
+++ intermédiaire1.php
@@ -4,12 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Vérification</title>
 </head>
 <body>
+    <?php
+        $Utilisateur =[["email" => "maxancemangeret@gmail.com", "nom"=>"Nakavasi","mdp"=>"1234"]];
+        $pseudo =$_POST["prenom"];
+        $mail= $_POST["mail"];
+        $mdp= $_POST["password"];
+        $nonValide= TRUE;
+        for ($i=0;$i<sizeof($Utilisateur);$i++){
+            if ($mail != $Utilisateur[$i]["email"]){
+                if ($pseudo != $Utilisateur[$i]["nom"]){
+                    if ($mdp != $Utilisateur[$i]["mdp"]){
+                        header('Location: accueil.html');
+                        $nonValide= FALSE;
+                    }
+                }
+            }
+        }
+        if ($nonValide){
+            header('Location: inscription.html');
+        }
+    ?>
 </body>
-bonjour
-<?php
-    include("connexion.html");
-?>
 </html>
