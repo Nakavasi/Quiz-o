14198431a1130ed898f4150d1d0c14943e195431
satosakisato <121882046+satosakisato@users.noreply.github.com>
2023-03-14 14:12:06 +0100

    reponse fait

--- reponse.php
+++ reponse.php
@@ -1,12 +1,32 @@
-<html>
+<html lang="fr">
 <head>
-<meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reponse.css">
+    <title>Quizz</title>
 </head>
 <body>
-<h1>Informations saisies</h1>
 <?php
 $titre =$_POST["titre"];
-echo "Titre: ".$titre."<br>";
-?> 
+?>
+<h1>Nom de Quizz</h1>
+<h2>Question</h2>
+<form action="reponse.php" method="post">
+    <div class="choix1choix2">
+        <input type="radio" id="choix1" name="choix" value="choix1">
+        <label for="choix1">Choix 1</label><br>
+        <input type="radio" id="choix2" name="choix" value="choix2">
+        <label for="choix2">Choix 2</label><br>
+    </div>
+    <div class="choix3choix4">
+        <input type="radio" id="choix3" name="choix" value="choix3">
+        <label for="choix3">Choix 3</label><br>
+        <input type="radio" id="choix4" name="choix" value="choix4">
+        <label for="choix4">Choix 4</label><br>
+    </div>
+    <div class="envoyer">
+        <input type="submit" name="envoie" value="Envoyer">
+    </div>
 </body>
 </html>
